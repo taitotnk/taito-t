e448611ac6aca2eb18dddd1c8ec3aa3aaac18d1b
Taito <taito.tanaka1211@gmail.com>
2021-03-10 00:23:04 +0900

    modify detail

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -35,16 +35,16 @@ export default function Home( {allPostsData}: {
       </Head>
           <h1 className={index_style.name}>I am Taito</h1>
           <div className={index_style.icon_cont}>
-          <a href="https://github.com/Taito-Code" target="_blank"><Image className={index_style.icon} src='/github-icon.png' height={70} width={70} alt="github-icon"/></a>
-          <a href="https://twitter.com/taito_1211" target="_blank"><Image className={index_style.icon} src='/twitter-icon.png' height={70} width={70} alt="twitter-icon"/></a>
-          <a href="https://www.instagram.com/taito_tanaka" target="_blank"><Image className={index_style.icon} src='/instagram-icon.png' height={70} width={70} alt="instagram-icon"/></a>
+          <a href="https://github.com/Taito-Code" target="_blank" rel="noopener"><Image className={index_style.icon} src='/github-icon.png' height={70} width={70} alt="github-icon"/></a>
+          <a href="https://twitter.com/taito_1211" target="_blank" rel="noopener"><Image className={index_style.icon} src='/twitter-icon.png' height={70} width={70} alt="twitter-icon"/></a>
+          <a href="https://www.instagram.com/taito_tanaka" target="_blank" rel="noopener"><Image className={index_style.icon} src='/instagram-icon.png' height={70} width={70} alt="instagram-icon"/></a>
           </div>
-          {allPostsData.map(({ id, title, created_at, updated_at ,thumbnail, tag }) => (
+          {allPostsData.map(({ id, title, created_at, thumbnail, tag }) => (
             <div className={index_style.post_list}>
             <div className="max-w-lg rounded overflow-hidden shadow-md my-2">
             <Image className="w-full" src={thumbnail}  height={300} width={600} alt="thumbnail"/>
             <div className="px-6 py-4">
-            <div className={index_style.title}>
+            <div className={index_style.title} key={id} >
             <Link href={`/posts/${id}`}>
             <a><div className="font-bold text-xl mb-2"><h2>{title}</h2></div></a>
             </Link>
@@ -52,7 +52,7 @@ export default function Home( {allPostsData}: {
             </div>
             <div className="px-0 py-0">
             <div className={index_style.tag}><span className="inline-block bg-grey-lighter rounded-full px-0 py-0 text-sm font-semibold text-grey-darker mr-0"><h1>#{tag}</h1></span></div>
-            <h3 className={index_style.date}>{created_at}</h3>
+            <h2 className={index_style.date}>{created_at}</h2>
             </div>
             </div>
             </div>
