ab11d470229c7b429a887b2bb63c8274ee0cd03a
Taito <taito.tanaka1211@gmail.com>
2021-03-11 11:32:42 +0900

    add gtag

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
+import { GA_TRACKING_ID } from '../lib/gtag'
 
 type Props = {}
 
@@ -6,7 +7,28 @@ class MyDocument extends Document<Props> {
   render() {
     return (
       <Html lang="ja">
-        <Head />
+        <Head>
+        {GA_TRACKING_ID != null && (
+        <>
+          {/* Global Site Tag (gtag.js) - Google Analytics */}
+        <script
+          async
+            src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+        />
+        <script
+          dangerouslySetInnerHTML={{
+           __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });`,
+        }}
+          />
+        </>
+        )}
+        </Head>
         <body>
           <Main />
           <NextScript />
