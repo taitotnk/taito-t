781b6b09dd6e6f6cfbe2471c5f77225729c77bb7
Taito-Code <taito.tanaka1211@gmail.com>
2021-09-23 12:39:01 +0900

    コードスタイルの変更とPostWhiteCardの幅改善

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,7 +5,7 @@ import { useRouter } from "next/router";
 import { useEffect } from "react";
 import React from "react";
 import * as gtag from "../lib/gtag";
-import "highlight.js/styles/rainbow.css";
+import "highlight.js/styles/atom-one-dark.css";
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   const router = useRouter();
