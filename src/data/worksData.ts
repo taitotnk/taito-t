5a48ea069eba98a3a84f495d134eb42f046f1576
Taito-Code <taito.tanaka1211@gmail.com>
2021-09-30 23:40:03 +0900

    worksデータの登録

---
+++ src/data/worksData.ts
@@ -0,0 +1,48 @@
+export type WorksType = {
+  appName: string;
+  img: string;
+  description: string;
+
+  url?: string;
+};
+
+const worksData: WorksType[] = [
+  {
+    appName: "Morphomusic",
+    img: "/images/Morphomusic.jpg",
+    description:
+      "LINE上のメッセージを形態素解析して曲を返してくれるBotサービスです! Djangoを使ってバックエンドを担当しました。",
+    url: "https://morphomusic.netlify.app/",
+  },
+
+  {
+    appName: "taito-t.com",
+    img: "/images/taito-t.jpg",
+    description: "このポートフォリオサイトです。Next.jsを採用しています。",
+    url: "https://taito-t.com/",
+  },
+
+  {
+    appName: "INIAD-MATCH",
+    img: "/images/iniad-match.jpg",
+    description:
+      "大学内でSNSアカウントを共有できるアプリです。※INIADアカウントでのみログインできます。",
+    url: "https://iniad-match.vercel.app/",
+  },
+
+  {
+    appName: "taito-blog",
+    img: "/images/taito-blog.jpg",
+    description:
+      "前ポートフォリオ&技術ブログサイトです。Djangoを使用しました。※現在は使用していません。",
+    url: "https://taito-blog.herokuapp.com/",
+  },
+
+  {
+    appName: "足袋紹介サイト",
+    img: "/images/gyoda-tabi.jpg",
+    description:
+      "地元の特産品であり、日本遺産にも登録されている足袋について紹介しているサイトです。",
+    url: "https://gyoda-tabi.vercel.app/",
+  },
+];
